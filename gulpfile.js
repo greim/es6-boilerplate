349e86c27fe6d1cac402e1bc98979522f1d2eb84
Greg Reimer <gregreimer@gmail.com>
2015-03-21 16:39:08 -0600

    Fixed license header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // --------------------------------------------------------
 // Copyright (c) 2015 by Greg Reimer <gregreimer@gmail.com>
-// MIT License. See mit-license.txt for more info.
+// MIT License. See license.txt for more info.
 
 /*
  * This is the entry point for running gulp. However,
